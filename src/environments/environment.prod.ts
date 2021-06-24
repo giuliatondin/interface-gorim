fdb99245942c3621de37bd4adae7aa7ba900f029
Vinicius <vimiciusbormar27@gmail.com>
2021-06-23 21:42:12 -0300

    Update API URL in Production Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  ApiUrl: 'http://localhost:8080',
-  WebSocketURL: 'ws://localhost:8080/ws'
+  ApiUrl: 'http://gorim.c3.furg.br:8080',
+  WebSocketURL: 'ws://gorim.c3.furg.br:8080/ws'
 };
